655e674c951cd2930311ff3662b7a6e7f4413a17
Dmitry <mr.blacky01@gmail.com>
2016-12-08 06:53:29 +0300

    +Add Mapper.Tests and basic tests

---
+++ MapperLibrary/MapperLibrary.UnitTests/Destination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperLibrary.UnitTests
+{
+    internal class Destination
+    {
+        public long FirstProperty { get; set; }
+        public string SecondProperty { get; set; }
+        public float ThirdProperty { get; set; }
+        public DateTime FourthProperty { get; set; }
+        public TempClass DtoTempClass { get; set; }
+        
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this == obj) return true;
+            var comparrigDestination = obj as Destination;
+            return comparrigDestination != null && IsElementsEqual(comparrigDestination);
+        }
+
+        private bool IsElementsEqual(Destination obj)
+        {
+            return this.FirstProperty == obj.FirstProperty &&
+                   this.SecondProperty == obj.SecondProperty &&
+                   this.ThirdProperty == obj.ThirdProperty &&
+                   this.FourthProperty == obj.FourthProperty &&
+                   this.DtoTempClass == obj.DtoTempClass;
+        }
+    }
+}
---
+++ MapperLibrary/MapperLibrary.UnitTests/MapperTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MapperLibrary.UnitTests
+{
+    [TestFixture]
+    internal class MapperTest
+    {
+        private static readonly Source Source = new Source()
+        {
+            FirstProperty = 5,
+            SecondProperty = "5",
+            ThirdProperty = 5.0,
+            FourthProperty = 1,
+            DtoTempClass = new TempSubClass()
+        };
+
+        private static readonly Destination ExpectedDestination = new Destination()
+        {
+            FirstProperty = 5,
+            SecondProperty = "5",
+            DtoTempClass = Source.DtoTempClass
+        };
+
+        [Test]
+        public void IsValidPatameter_Null_Exception()
+        {
+            Mapper mapper = new Mapper();
+
+            Assert.Catch<ArgumentNullException>(() => mapper.Map<object, object>(null));
+        }
+
+        [Test]
+        public void Mapping_SimpleMapping_Equal()
+        {
+            Mapper mapper = new Mapper();
+
+            Destination dest = mapper.Map<Source, Destination>(Source);
+
+            Assert.AreEqual(ExpectedDestination, dest);
+        }
+
+        [Test]
+        public void Mapping_NoCompatibilityProperties_ReturnsEmpty()
+        {
+            Mapper mapper = new Mapper();
+
+            Destination dest = mapper.Map<TempClass, Destination>(new TempClass() );
+
+            Assert.AreEqual(new Destination(), dest);
+        }
+
+        [Test]
+        public void Mapping_WithCache_Equals()
+        {
+            Mapper mapper = new Mapper();
+            mapper.Map<Source, Destination>(Source);
+
+            Destination temp = mapper.Map<Source, Destination>(Source);
+
+            Assert.AreEqual(ExpectedDestination, temp);
+        }
+
+
+
+    }
+}
