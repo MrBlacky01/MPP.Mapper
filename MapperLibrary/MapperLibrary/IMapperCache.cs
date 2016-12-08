8af9d1b43870eca8d5bff589eafc26ab0bb99ec6
Dmitry <mr.blacky01@gmail.com>
2016-12-08 04:43:51 +0300

    +Add interfaces for cache and function creators

---
+++ MapperLibrary/MapperLibrary/IMapperCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperLibrary
+{
+    public interface IMapperCache
+    {
+        void Add(MappingPair pair, Delegate function);
+        Delegate GetCache (MappingPair pair);
+        bool Contains(MappingPair pair);
+    }
+}
---
+++ MapperLibrary/MapperLibrary/IMapperFunctionCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperLibrary
+{
+    interface IMapperFunctionCreator
+    {
+        Func<TSource, TDestination> CreateFunction <TSource, TDestination>(IEnumerable<MappingPropertyInfoPair> properties)
+            where TDestination : new();
+    }
+}
